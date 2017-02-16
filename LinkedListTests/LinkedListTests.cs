9b99d830dc4001f5a2510311f6bb0a8cc4fdfbe0
Oleh Pedorenko <olezhkoo@gmail.com>
2017-02-16 18:21:46 +0200

    Improved test coverage.

--- LinkedListTests/LinkedListTests.cs
+++ LinkedListTests/LinkedListTests.cs
@@ -42,31 +42,102 @@ namespace LinkedList.Tests
         [TestMethod]
         public void AddAtTest()
         {
-            Assert.Fail();
+            LinkedList<int> linkedList = new LinkedList<int>(1, 2, 3, 4, 5);
+            linkedList.AddAt(8, 2);
+            Assert.AreEqual(2, linkedList.ElementAt(1));
+            Assert.AreEqual(8, linkedList.ElementAt(2));
+            Assert.AreEqual(3, linkedList.ElementAt(3));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void AddAtNegativeRangeTest()
+        {
+            LinkedList<int> linkedList = new LinkedList<int>(1, 2, 3, 4, 5);
+            linkedList.AddAt(8, -1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void AddOutOfRangeTest()
+        {
+            LinkedList<int> linkedList = new LinkedList<int>(1, 2, 3, 4, 5);
+            linkedList.AddAt(8, 6);
         }
 
         [TestMethod]
         public void RemoveTest()
         {
-            Assert.Fail();
+            LinkedList<int> linkedList = new LinkedList<int>(1, 2, 3, 4, 5);
+            linkedList.Remove();
+            Assert.AreEqual(4, linkedList.Length);
+            Assert.AreEqual(2, linkedList.ElementAt(0));
         }
 
         [TestMethod]
         public void RemoveAtTest()
         {
-            Assert.Fail();
+            LinkedList<int> linkedList = new LinkedList<int>(1, 2, 3, 4, 5);
+            linkedList.RemoveAt(2);
+            Assert.AreEqual(4, linkedList.Length);
+            Assert.AreEqual(4, linkedList.ElementAt(2));
+            Assert.AreEqual(2, linkedList.ElementAt(1));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void RemoveAtNegariveRangeTest()
+        {
+            LinkedList<int> linkedList = new LinkedList<int>(1, 2, 3, 4, 5);
+            linkedList.RemoveAt(-1);
+            Assert.AreEqual(4, linkedList.Length);
+            Assert.AreEqual(4, linkedList.ElementAt(2));
+            Assert.AreEqual(2, linkedList.ElementAt(1));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void RemoveAtOutOfRangeTest()
+        {
+            LinkedList<int> linkedList = new LinkedList<int>(1, 2, 3, 4, 5);
+            linkedList.RemoveAt(6);
         }
 
         [TestMethod]
         public void ElementAtTest()
         {
-            Assert.Fail();
+            LinkedList<int> linkedList = new LinkedList<int>(1, 2, 3, 4, 5);
+            Assert.AreEqual(1, linkedList.ElementAt(0));
+            Assert.AreEqual(3, linkedList.ElementAt(2));
+            Assert.AreEqual(5, linkedList.ElementAt(4));
         }
 
         [TestMethod]
-        public void GetEnumeratorTest()
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void ElementAtNegativeRangeTest()
         {
-            Assert.Fail();
+            LinkedList<int> linkedList = new LinkedList<int>(1, 2, 3, 4, 5);
+            linkedList.ElementAt(-1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void ElementAtOutOfRangeTest()
+        {
+            LinkedList<int> linkedList = new LinkedList<int>(1, 2, 3, 4, 5);
+            linkedList.ElementAt(6);
+        }
+
+        [TestMethod]
+        public void ForeachTest()
+        {
+            LinkedList<int> linkedList = new LinkedList<int>(1, 2, 3, 4, 5);
+            string s = "";
+            foreach (var i in linkedList)
+            {
+                s += i;
+            }
+            Assert.AreEqual("12345", s);
         }
     }
 }
